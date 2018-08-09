7a543a90a9ec3c38ec98bcb596d43cd0453b3bf4
Litzy Bucio <lbucio2@cps.edu>
2018-08-09 18:22:16 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -12,6 +12,9 @@ end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  updated_hash = create_olympics_hash
+  updated_hash[:Atlanta] = "1996"
+  return updated_hash
 end
 
 def iterate_through_hash
